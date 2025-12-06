24d272387675a500d750d3c8cfebdae148ec633f
Shra1t <Flacko2018@mail.ru>
2025-12-06 11:26:17 +0500

    разработанный класс dish. ilm

---
+++ Classes/Dish.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eliza13pr.Classes
+{
+    public class Dish
+    {
+        public int id;
+        public string name;
+        public List<Sizes> sizes = new List<Sizes> { };
+        public string img;
+        public List<Ingredient> ingredients = new List<Ingredient> { };
+        public int activeSize = 0;
+        public class Sizes
+        {
+            public int id;
+            public int id_size;
+            public int price;
+            public int wes;
+        }
+        public class Ingredient
+        {
+            public int id;
+            public string name;
+            public int price;
+            public int wes;
+            public string img;
+            public int count;
+
+        }
+
+    }
+}
